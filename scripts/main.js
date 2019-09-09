260de3a4cc22e06a9ce7c4794f0d8110ccd72978
Juan Bravo <juanda.bravo98@gmail.com>
2019-09-09 08:07:09 -0500

    JS color interaction

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,20 @@
 function handleLoad() {
-    var buttonNext = document.querySelector('.next');
-    var buttonPrev = document.querySelector('.prev');
-    var container = document.querySelector('.gallery');
-    var slider = container.querySelector('.gallery__tape');
-    var count = 0;
 
-    var first = slider.querySelector('.gallery__container');
-    var firstImg = first.querySelector('img');
-    var newLast = document.createElement('div');
-    var newLastImg = newLast.createElement('img')
-    newLastImg.setAttribute('src', firstImg.getAttribute('src'));
-    newLast.classList.add('gallery__container');
-    newLastImg.classList.add('gallery_img');
-    slider.append(newLast);
+    var colorButtons = document.querySelectorAll('.inter__color');
+    var productImage = document.querySelector('.productModel');
 
-    function handleLast(){
-        console.log('cambio');
-        slider.classList.add('gallery__tape--inactive');
-        slider.style.transform = 'translate(0px, 0px)';
+    function iterateButtons(btn, index) {
+
+        function handleColorClick() {
+
+            productImage.setAttribute('src', './styles/images/model'+index+'.png');
+           
+        }
+
+        btn.addEventListener('click', handleColorClick);
     }
 
-    function handleNextClick(event) {
-        count++;
-    
-        if(count == 1){
-            slider.classList.remove('gallery__tape--inactive');
-        }
-    
-        var mov = slider.offsetWidth * -1 * count;
-        slider.style.transform = 'translate(' + mov + 'px, 0px)';
-        
-        if(count > slider.childElementCount - 2){
-            setTimeout(handleLast, 300);
-            count = 0;
-        }
-    }
-
-    
-
-    buttonNext.addEventListener('click', handleNextClick);
-
+    colorButtons.forEach(iterateButtons);
 }
 
 window.addEventListener('load', handleLoad);
